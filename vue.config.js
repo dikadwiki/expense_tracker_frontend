c77a8b75bcb1a1cb303abd2ee178ae8de316bd07
dikadwiki <dwikinoviandika24@gmail.com>
2026-09-23 20:17:38 +0700

    config fix

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ module.exports = {
   devServer: {
     port: 8080,
     host: '0.0.0.0', // Allow access from Docker network
-    watchOptions: {
-      poll: true // Enable polling for Docker volume mounting
-    }
   },
   // Public path for production build
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
